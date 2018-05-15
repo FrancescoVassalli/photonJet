e078c801167ecc8f819d05af8bb4421b82faf242
FrancescoVassalli <Francesco.Vassalli@colorado.edu>
2018-05-15 09:06:48 -0600

    first

---
+++ Utils.C
@@ -0,0 +1,159 @@
+template<class T>
+	T quadrature(T d1, T d2){
+		return TMath::Sqrt((double)d1*d1+d2*d2);
+	}
+	template<class T>
+	T quadrature(T* a, int SIZE){
+		T* b = clone(a);
+		arrayMultiply(b,b,SIZE);
+		T r = sum(b,SIZE);
+		return TMath::Sqrt(r);
+
+	}
+class Scalar
+{
+public:
+	Scalar(){
+		value=uncertainty=0;
+	}
+	Scalar(float value){
+		this->value=value;
+		uncertainty=0;
+	}
+	Scalar(float value, float uncertainty){
+		this->value=value;
+		this->uncertainty=uncertainty;
+	}
+	~Scalar(){}; 
+	//operators might need to return class types but I'm not sure
+	Scalar operator+(const Scalar &s){
+		//cout<<"Addition "<<value<<" + "<<s.value<<"\n";;
+		Scalar next = Scalar();
+		next.value = this->value + s.value;
+		next.uncertainty = quadrature(this->uncertainty, s.uncertainty);
+		return next;
+	}
+	Scalar operator/(const Scalar &s){
+		Scalar next = Scalar();
+		next.uncertainty = (value/s.value)*quadrature(this->uncertainty/value, s.uncertainty/s.value);
+		next.value = value/s.value;
+		if (value==0||s.value==0)
+		{
+			next.uncertainty=0;
+		}
+		return next;
+	}
+	void operator+=(Scalar s){
+		value+=s.value;
+		uncertainty = quadrature(this->uncertainty, s.uncertainty);
+	}
+	void operator*=(Scalar s){
+		*this = *this * s;
+	}
+	void operator/=(Scalar s){
+		*this = *this / s;
+	}
+	void operator-=(Scalar s){
+		*this = *this - s;
+	}
+	Scalar operator+(float s){
+		Scalar next;
+		next.value = this->value + s;
+		return next;
+	}
+	Scalar operator-(Scalar s){
+		Scalar next;
+		next.value = this->value - s.value;
+		next.uncertainty = quadrature(this->uncertainty, s.uncertainty);
+		return next;
+	}
+	Scalar operator-(float s){
+		Scalar next;
+		next.value = this->value - s;
+		return next;
+	}
+	void operator=(Scalar s){
+		value=s.value;
+		uncertainty=s.uncertainty;
+	}
+	bool operator==(Scalar s){
+		return value==s.value;
+	}
+	bool operator>(Scalar s){
+		return value>s.value;
+	}
+	bool operator<(Scalar s){
+		return value<s.value;
+	}
+	bool operator>=(Scalar s){
+		return value>=s.value;
+	}
+	bool operator<=(Scalar s){
+		return value<=s.value;
+	}
+	bool operator!=(Scalar s){
+		return value!=s.value;
+	}
+	Scalar operator*(Scalar s){
+		Scalar next;
+		next.uncertainty = (value*s.value)*quadrature(this->uncertainty/value, s.uncertainty/s.value);
+		if (value==0||s.value==0)
+		{
+			next.uncertainty=0;
+		}
+		next.value = value*s.value;
+		return next;
+	}
+	Scalar operator*(float s){
+		Scalar next;
+		next.value = value*s;
+		next.uncertainty = (value*s)*uncertainty/value;
+		if (value==0||s==0)
+		{
+			next.uncertainty=0;
+		}
+		return next;
+	}
+	Scalar operator/(float s){ //progate uncertainty
+		Scalar next;
+		next.uncertainty=(value/s)*uncertainty/value;
+		if (value==0)
+		{
+			next.uncertainty=0;
+		}
+		next.value = value/s;
+		return next;
+	}
+	Scalar pow(double n){
+		Scalar r;
+		if (n==0)
+		{
+			r.value=1;
+			r.uncertainty=0;
+			return r;
+		}
+		r.value = TMath::Power((double)value,n);
+		r.uncertainty=(r.value*n*uncertainty/value);
+		return r;
+	}
+	Scalar log(float base){
+		Scalar r;
+		r.value = TMath::Log(value)/TMath::Log(base);
+		r.uncertainty = uncertainty/(value*TMath::Log(base));
+		return r;
+	}
+
+	friend ostream& operator<<(ostream& os, Scalar const & tc) {
+        return os <<"Scalar: " << tc.value <<char(241)<<tc.uncertainty<<'\n';
+    }
+
+float value;
+float uncertainty;
+	
+};
+
+struct Point
+{
+	Scalar x;
+	Scalar y;
+};
---
+++ photonjetmaker.cc
@@ -0,0 +1,178 @@
+/*
+	for the generation of photon-jet observables through Pythia pp collisions 
+	by Francesco Vassalli
+*/
+
+#include "Pythia8/Pythia.h"
+using namespace Pythia8;
+using namespace std;
+#include "TFile.h"
+#include "TTree.h"
+#include "TRandom3.h"
+#include "TMath.h"
+#include "Utils.C"
+
+class Photon
+{
+public:
+	Photon(){}
+	Photon(float _pT){
+		this->pT = Scalar(_pT);
+	}
+	Photon(float _pT,float _phi){
+		this->pT = Scalar(_pT);
+		this->phi= Scalar(_phi);
+	}
+	Photon(float _pT,float _phi, float _eta){
+		this->pT = Scalar(_pT);
+		this->phi= Scalar(_phi);
+		this->eta= Scalar(_eta);
+	}
+	~Photon(){}
+	Scalar getpT(){
+		return pT;
+	}
+	void setpT(float _pT){
+		this->pT = Scalar(_pT);
+	}
+	Scalar getphi(){
+		return phi;
+	}
+	void setphi(float _phi){
+		this->phi = Scalar(_phi);
+	}
+	Scalar geteta(){
+		return eta;
+	}
+	void seteta(float _eta){
+		this->eta = Scalar(_eta);
+	}
+	Point getAngle(){
+		Point r;
+		r.x=phi;
+		r.y=eta;
+		return r;
+	}
+
+private:
+	Scalar pT;
+	Scalar phi;
+	Scalar eta;
+
+};
+
+class Jet
+{
+public:
+	Jet(){}
+	Jet(float _pT, float _phi, float _y, float _r){
+		this->pT =_pT;
+		this->phi = _phi;
+		this->y = _y;
+		this->r = _r;
+	}
+	~Jet(){
+		if (next!=NULL)
+		{
+			delete next;
+			next =NULL;
+		}
+		if (pair!=NULL)
+		{
+			delete pair;
+			pair=NULL;
+		}
+	}
+	void setMult(int m){
+		mult=m;
+	}
+	int getmult(){
+		return mult;
+	}
+	float getpT(){
+		return pT;
+	}
+	float getphi(){
+		return phi;
+	}
+	float gety(){
+		return y;
+	}
+	float getr(){
+		return r;
+	}
+private:
+	float pT=-1;
+	float phi =7;
+	float y=0;
+	float r=-1;
+	int mult=0;
+	Jet* next=NULL;
+	Jet* pair=NULL;
+	
+};
+
+void makeData(std::string filename, int nEvents){
+	TFile* f = new TFile(filename.c_str(),"RECREATE");
+  	TTree* t=new TTree("tree100","events");
+
+  	/*pythia set up*/
+  	Pythia pythiaengine;
+  	pythiaengine.readString("Beams:eCM = 2760.");
+ 	pythiaengine.readString("promptphoton:all = on");
+ 	pythiaengine.readString("HardQCD:all = on");
+ 	pythiaengine.readString("PhaseSpace:pTHatMin = 10.");
+ 	pythiaengine.readString("Random::setSeed = on");
+  	pythiaengine.readString("Random::seed =0");
+  	pythiaengine.init();
+
+  	/* Tbranching  */
+  	SlowJet *antikT = new SlowJet(-1,.4,10,4,2,1);
+  	Jet *temp =NULL;
+  	int finalGammaCount=0;
+  	/* generation loop*/
+  	for (int iEvent = 0; iEvent < nEvents; ++iEvent)
+  	{
+  		if (!pythiaengine.next()){
+      		cout<<"pythia.next() failed"<<"\n";
+      		iEvent--;
+      		continue;
+    	}
+    	if(iEvent%30==0) {
+  			cout<<"Event N: "<<iEvent<<'\n';
+    	}
+    	/* zero out */
+    	for (int i = 0; i < pythiaengine.event.size(); ++i)
+    	{
+    		if (pythiaengine.event[i].id()==22&&pythiaengine.event[i].isFinal()&&pythiaengine.event[i].pT()>10&&TMath::Abs(pythiaengine.event[i].eta())<1)
+    		{
+    			//get the generation process 
+    			//cout<<pythiaengine.process[i].pT()<<'\n';
+    			Photon myPhoton = Photon(pythiaengine.event[i].pT(),pythiaengine.event[i].phi(),pythiaengine.event[i].eta());
+    			antikT->analyze(pythiaengine.event);
+    			cout<<"Photon phi:"<<pythiaengine.event[i].phi()<<" Jet1 phi:"<<antikT->phi(0)<<" Jet2 phi:"<<antikT->phi(1)<<"\n";
+    			cout<<"Photon pT:"<<pythiaengine.event[i].pT()<<" Jet1 pT:"<<antikT->pT(0)<<" Jet2 pT:"<<antikT->pT(1)<<"\n";
+     			finalGammaCount++;
+    		}
+    	}
+    	/*
+    	std::vector<Jet> myJets(0);
+    	for (int i = 0; i < antikT->sizeJet(); ++i)
+    	{
+    		temp = new Jet(antikT->pT(i),antikT->phi(i),antikT->y(i),.4);
+    		// if mult is needed add here
+    		myJets.push_back(temp);
+
+    	}*/
+  	}
+  	cout<<finalGammaCount<<endl;
+}
+
+
+int main()
+{
+	string fileOut = "";
+	int nEvents = 1000;
+	makeData(fileOut,nEvents);
+	return 0;
+}
